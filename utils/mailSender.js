dde08fd6791c34507a05b14a936723d3ceae63f2
mohammedrimshan <rimshanshanu55@gmail.com>
2024-12-28 15:39:31 +0530

    Changes made

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -4,8 +4,8 @@ const mailSender = async (email, subject, htmlContent) => {
   try {
     let transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
